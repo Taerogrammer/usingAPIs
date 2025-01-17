29722b18a1b5ca90e0faf6ee97874be2210d1264
Taerogrammer <hyung54321@naver.com>
2025-01-17 16:43:43 +0900

    feat: SPM 다운 및 프로토콜 구현

---
+++ 0117_Networks/0117_Networks/Protocol/Protocol.swift
@@ -0,0 +1,12 @@
+//
+//  Protocol.swift
+//  0117_Networks
+//
+//  Created by 김태형 on 1/17/25.
+//
+
+protocol ViewCofiguration {
+    func configureHierarchy()
+    func configureLayout()
+    func configureView()
+}
